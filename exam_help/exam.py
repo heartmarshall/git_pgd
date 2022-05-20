2df63ecfdd3777762faa0ebe6cb76f02d939b743
heartmarshall <heartmarshall@maill.ru>
2022-05-20 12:53:45 +0300

    test

--- exam_help/exam.py
+++ exam_help/exam.py
@@ -1,21 +1,29 @@
 import pyautogui
 import time
 import os
-
+import subprocess
 
 PICS_DIR = os.path.join(os.getcwd(), "pics")
 SEND_TO_GIT_SCRIPT = os.path.join(os.getcwd(), "send.sh")
 os.makedirs(PICS_DIR, exist_ok=True)
 
 DELAY = 0  # Задержка перед тем, как программа начёнет делать скриншоты
-LOOPS_NUM = 4  # Сколько раз будет сделан скриншот
+LOOPS_NUM = 10  # Сколько раз будет сделан скриншот
 SLEEP_TIME = 1  # Промежуток в секундах между скриншотами
 
 time.sleep(DELAY)
 
-for screenshot_num in range(LOOPS_NUM):
+dir_num = 0
+
+for screenshot_num in range(1, LOOPS_NUM+1):
+    cur_dir = os.path.join(PICS_DIR, str(dir_num))
+    os.makedirs(cur_dir, exist_ok=True)
     myScreenshot = pyautogui.screenshot()
-    myScreenshot.save(os.path.join(PICS_DIR, str(screenshot_num) + '.png'))
+    myScreenshot.save(os.path.join(cur_dir, str(screenshot_num) + '.png'))
     time.sleep(SLEEP_TIME)
+    if screenshot_num % 5 == 0:
+        dir_num += 1
+        subprocess.run(args=[SEND_TO_GIT_SCRIPT, str(dir_num)], shell=True)
+        # os.system(SEND_TO_GIT_SCRIPT)
 
 os.system(SEND_TO_GIT_SCRIPT)
--- exam_help/send.sh
+++ exam_help/send.sh
@@ -1,3 +1,3 @@
-git add ./pics/*
+git add ./pics/$1/*
 git commit -m 'New Pics'
 sudo git push
